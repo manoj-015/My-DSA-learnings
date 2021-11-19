40bb88194684e5aef3ac0ced96c5da6b6f8266a0
Manoj <65275028+MANOJ-015@users.noreply.github.com>
2021-11-19 17:13:38 +0530

    Create Bit_Manipulation.cpp

---
+++ Number Theory/Bit_Manipulation.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+void checkBit(int n, int i)
+{
+	((n >> i) &1) ? cout << "Set Bit": cout << "Unset Bit";
+}
+
+int setBit(int n, int i)
+{
+	return ((1 << i) | n);
+}
+
+int unsetBit(int n, int i)
+{
+	return ((~(1 << i)) &n);
+}
+
+void printBinary(int n)
+{
+	for (int i = 10; i >= 0; --i)
+	{
+		cout << ((n >> i) &1);
+	}
+
+	cout << endl;
+}
+
+int countNumberOfSetBits(int n)
+{
+	int count = 0;
+	for (int i = 30; i >= 0; --i)
+	{
+		count += (n >> i) &1;
+	}
+
+	return count;
+}
+
+int toggleBit(int n, int i)
+{
+	return (n ^ (1 << i));
+}
+
+int main()
+{
+	int n, i;
+	cin >> n >> i;
+	printBinary(n);
+
+	n = toggleBit(n, i);
+	printBinary(n);
+
+	return 0;
+}
