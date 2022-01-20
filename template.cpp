585445eed1f501bde47aec7d38f54ad778c165ef
Manoj <65275028+MANOJ-015@users.noreply.github.com>
2022-01-20 18:33:42 +0530

    Create template.cpp
    Created and added the template code into the file

---
+++ template.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<unordered_set>
+#include<unordered_map>
+#include<math.h>
+
+
+using namespace std;
+
+#define ll long long
+#define skip continue
+
+#define MAXL 9223372036854775807  // 10^18
+#define MAXI 2147483647           //10^9
+#define MINL -9223372036854775808 // 10^18
+#define MINI -2147483648           // 10^9
+#define MAXUL 18446744073709551615 // 10^19
+#define MAXUI 4294967295           //10^9
+
+#define MOD 1000000007 //1000000007 (10^9+7)
+#define MOD2 10007
+#define MAX 100005
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+
+	int test;
+	cin >> test;
+
+	while(test--)
+	{
+		// code goes here
+	}
+	return 0;
+}
